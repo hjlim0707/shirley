ca9c454cec31efd3f68184bd3b1d2e2f629f2825
mindyyuan <mindyuan@gmail.com>
2017-12-27 20:27:17 -0500

    home page style adjustments

--- src/components/home_header/index.jsx
+++ src/components/home_header/index.jsx
@@ -13,7 +13,7 @@ export default class HomeHeader extends React.PureComponent {
 
   leftContent() {
     return this.state.leftContent === 'full' ? (
-      <div className='header-description col-12 col-md-5'>
+      <div className='header-description col-11 col-md-5'>
         <a className='name' onClick={this.onClick}>Shirley Huong </a> is a product designer based in Brooklyn, NYC. She currently works at <Link text='Paperless Post' href='https://www.paperlesspost.com' />,
         focusing on Mobile Products.
       </div>
@@ -48,7 +48,7 @@ export default class HomeHeader extends React.PureComponent {
       <div className={`home-header container-fluid ${this.state.leftContent}`}>
         <div className='row'>
           {this.leftContent()}
-          <div className={`ctas col-12 col-md-4 ${this.state.leftContent === 'full' ? 'offset-md-3' : null}`}>
+          <div className={`ctas col-12 col-md-4 ${this.state.leftContent === 'full' ? 'offset-md-3' : 'condensed'}`}>
             <Link text='Email' href='mailto:huongshirley@gmail.com'/>
             <Link text='Resume' href='../../assets/Shirley_Huong_Resume.pdf' />
             <Link text='IG' href='http://www.instagram.com/shirleyisfishing'/>
